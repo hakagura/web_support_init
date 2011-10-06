34822ce2fa705d84c9d8f35fe0ec1bbe456d6d77
esmerino costa <hakagura@gmail.com>
2011-10-06 09:22:53 -0300

    Adding controller

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,62 @@
+class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+    end
+  end
+
+  def new
+    @ticket = Ticket.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def create
+    @ticket = Ticket.new(params[:ticket])
+
+    respond_to do |format|
+      if @ticket.save
+        format.html { redirect_to(@ticket, :notice => 'Ticket criado com sucesso.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+
+    respond_to do |format|
+      if @ticket.update_attributes(params[:ticket])
+        format.html { redirect_to(@ticket, :notice => 'Ticket atualizado com sucesso.') }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(tickets_url) }
+    end
+  end
+end
