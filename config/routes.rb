5127091f56bc4e596a94a9ae175a7fd1c82b2d13
esmerino costa <hakagura@gmail.com>
2011-10-06 09:22:15 -0300

    Remove issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SupportSystem::Application.routes.draw do
-  resources :issues
+  resources :tickets
 
+  root :to => "tickets#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +51,6 @@ SupportSystem::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "issues#index"
 
   # See how all your routes lay out with "rake routes"
 
