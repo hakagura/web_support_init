7ce8715b2f8d87674f5cd3374a6039a37881b693
Elias Ferreira Junior <eliaswebdev@gmail.com>
2011-10-10 15:47:29 -0300

    remove migrations

--- db/migrate/20111006205704_add_ticket_id_to_comments.rb
+++
@@ -1,9 +0,0 @@
-class AddTicketIdToComments < ActiveRecord::Migration
-  def self.up
-  	add_column :comments, :ticket_id, :integer
-  end
-
-  def self.down
-	remove_column :comments, :ticket_id
-  end
-end
