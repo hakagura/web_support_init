a280215de6155030affed0427e9b15a6db4260de
esmerino costa <hakagura@gmail.com>
2011-10-12 15:50:48 -0300

    Addind actions on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-if User.count == 0
-  puts "Creating admin user"
-  User.create([{:role=>'admin'},{:email=>'leilao@leilao.com.br'},{:password => 'leilao123456'}])
-  User.create([{:role=>'user'},{:email=>'junior@leilao.com.br'},{:password => 'leilao'}])
-end
-
+puts "Admin"
+User.create!(:role=>"admin",:email=>"admin@leilao.com.br",:password=>"leilao123456")
+puts "User"
+User.create!(:role=>"user",:email=>"junior@leilao.com.br",:password=>"leilao123456")
+puts "User"
+User.create!(:role=>"user",:email=>"hakagura@leilao.com.br",:password=>"leilao123456")
