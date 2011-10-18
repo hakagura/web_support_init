97ef6891fb75855613de6904389eef74eb09bba2
Rogerio <argerim@gmail.com>
2011-10-18 10:30:22 -0200

    atualizando bugs de migrations.

--- db/migrate/20111017145430_add_developer_id_to_ticket.rb
+++
@@ -1,9 +0,0 @@
-class AddDeveloperIdToTicket < ActiveRecord::Migration
-  def self.up
-    add_column :tickets, :developer_id, :integer
-  end
-
-  def self.down
-    remove_column :tickets, :developer_id
-  end
-end
